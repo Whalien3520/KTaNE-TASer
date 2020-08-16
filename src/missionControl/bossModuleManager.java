c0750e00fd8ca19c4f706b317ab7d7feeb577969
shortc1rcuit <declan@themcmoos.net>
2020-08-16 21:50:36 +0100

    Added src

---
+++ src/missionControl/bossModuleManager.java
@@ -0,0 +1,9 @@
+package missionControl;
+import abstraction.*;
+import java.util.ArrayList;
+public class bossModuleManager {
+	private ArrayList<bossModule> bossMods;
+	public bossModuleManager(String s) {
+	}
+	public boolean needInput() {return false;}
+}
