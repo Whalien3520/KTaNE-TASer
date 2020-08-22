f36bd1d20224970810b5291b98ffd5d6d5521672
makebao <markusbao.no1@gmail.com>
2020-08-22 00:18:49 -0500

    1.4.4 testing webhook again

--- src/missionControl/bossModuleManager.java
+++ src/missionControl/bossModuleManager.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class bossModuleManager {
 	private ArrayList<bossModule> bossMods;private edgework e;private bombConfig b;
 	public bossModuleManager(bombConfig bc, edgework ed) {e=ed;b=bc;
-	String input = JOptionPane.showInputDialog("Enter the number of B/BH:");
+	String input = JOptionPane.showInputDialog("Enter boss modules, separated by commas.");
 	}
 	public boolean needInput() {return false;}
 }
