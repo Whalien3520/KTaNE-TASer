841ee3c61f8cc7870ea418d22443f17279614e37
makebao <markusbao.no1@gmail.com>
2020-09-10 09:00:08 -0500

    1.6.1: Added Turn The Keys functionality as a printed set of instructions

--- src/missionControl/gameState.java
+++ src/missionControl/gameState.java
@@ -13,9 +13,13 @@ public class gameState {bombConfig b;edgework e;ArrayList<String> souvNotes;modu
 	}
 	public void bossModules(String s){/*String[]r=s.split(",");for(int i=0;i<r.length;i++);*/}/*incomplete*/
 	public void input(String s) {
-		/*if(bossManager.needInput()){}
-		else*/
 		if(s.equals("bossInput"))bossManager.input();
+		else if(s.equals("Turn The Keys"))System.out.println("Please solve all Morse Code, Wires, Two Bits, The Button, Colour Flash, and Round Keypad modules,/n"
+				+"while avoiding all Semaphore, Combination Lock, Simon Says, Astrology, Switches, and Plumbing modules,/n" 
+				+"then turn all right-side keys in descending number order./n"
+				+"Then, please solve all Password, Who's On First, Crazy Talk, Keypad, Listening, and Orientation modules,/n"
+				+"while avoiding all Maze, Memory, Comlicated Wires, Wire Sequence, and Cryptography modules,/n"
+				+"then turn all left-side keys in ascending number order.");
 		else if(s.equals("bossOutput"))bossManager.output();
 		else if(current!=null) {
 			if(current.isSolved()){current=null;identifyModule(s);}
